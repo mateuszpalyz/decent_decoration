7e776f34c3aa7866ef6c62093adc4b4a5b1879d3
Tomasz Pewiński <pewniak747@gmail.com>
2012-11-17 18:20:17 +0100

    added .decorate method implementation

--- lib/decent_decoration/decorate.rb
+++ lib/decent_decoration/decorate.rb
@@ -1,5 +1,12 @@
 module DecentDecoration
   module Decorate
+    def decorate(name, options = nil, &block)
+      decorator_class = "#{name.to_s.singularize.classify}Decorator".constantize
+      decorated_name = name.to_sym
+      undecorated_name = "undecorated_#{name}".to_sym
 
+      expose(undecorated_name, options, &block)
+      expose(decorated_name) { public_send(undecorated_name) }
+    end
   end
 end
