3da02cc5865c1730e4037b5466b0f3f6ba4aee59
Tomasz Pewiński <pewniak747@gmail.com>
2012-11-18 13:50:22 +0100

    add one more spec

--- spec/fixtures/decorators.rb
+++ spec/fixtures/decorators.rb
@@ -1,4 +1,7 @@
 require 'delegate'
 
 class ConferenceDecorator < SimpleDelegator
+  def decorated_object
+    __getobj__
+  end
 end
--- spec/integration/rails_application_spec.rb
+++ spec/integration/rails_application_spec.rb
@@ -3,8 +3,13 @@ require 'fixtures/controllers'
 require 'rspec/rails'
 
 describe ConferencesController, type: :controller do
-  it "should have decorated conference" do
+  it "should be a decorator" do
     get '/conference/RuPy'
     controller.conference.should be_instance_of(ConferenceDecorator)
+  end
+
+  it "should have decorated conference" do
+    get '/conference/RuPy'
+    controller.conference.decorated_object.should be_instance_of(Conference)
   end
 end
