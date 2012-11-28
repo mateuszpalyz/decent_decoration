e329935222dd80716841341c61ff4c348a3d9751
Tomasz Pewiński <pewniak747@gmail.com>
2012-11-28 13:28:11 +0100

    support for Draper decorators

--- lib/decent_decoration/decorate.rb
+++ lib/decent_decoration/decorate.rb
@@ -6,8 +6,9 @@ module DecentDecoration
       undecorated_name = "undecorated_#{name}".to_sym
 
       options[:model] ||= decorated_name
+      decorate_method = decorator_class.respond_to?(:decorate) ? :decorate : :new
       expose(undecorated_name, options, &block)
-      expose(decorated_name) { decorator_class.new(public_send(undecorated_name)) }
+      expose(decorated_name) { decorator_class.public_send(decorate_method, public_send(undecorated_name)) }
     end
   end
 end
