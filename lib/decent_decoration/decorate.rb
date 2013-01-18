1a2847e40a34b2beda35a778f65b3d45b961e485
Bartosz Kopiński <bartosz.kopinski@netguru.pl>
2013-01-18 01:36:19 +0100

    Use decorate_collection if plural

--- lib/decent_decoration/decorate.rb
+++ lib/decent_decoration/decorate.rb
@@ -1,14 +1,21 @@
 module DecentDecoration
   module Decorate
     def expose_decorated(name, options = {}, &block)
-      decorator_class = options.delete(:decorator) || "#{name.to_s.singularize.classify}Decorator".constantize
-      decorated_name = name.to_sym
-      undecorated_name = "undecorated_#{name}".to_sym
+      options[:model] ||= name
+      original_name = name.to_s
+      plural_name = original_name.pluralize
+      singular_name = original_name.singularize
+      decorator_class = options.delete(:decorator) || "#{options[:model].to_s.classify}Decorator".constantize
 
-      options[:model] ||= decorated_name
-      decorate_method = decorator_class.respond_to?(:decorate) ? :decorate : :new
+      if plural_name == original_name && plural_name != singular_name && decorator_class.respond_to?(:decorate_collection)
+        decorate_method = :decorate_collection
+      else
+        decorate_method = decorator_class.respond_to?(:decorate) ? :decorate : :new
+      end
+
+      undecorated_name = "undecorated_#{name}".to_sym
       expose(undecorated_name, options, &block)
-      expose(decorated_name) { decorator_class.public_send(decorate_method, public_send(undecorated_name)) }
+      expose(name) { decorator_class.public_send(decorate_method, public_send(undecorated_name)) }
     end
   end
 end
