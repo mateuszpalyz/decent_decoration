cd2e8df7554a0e2db8c49ad2609bcad9d00f6d6e
Tomasz Pewiński <pewniak747@gmail.com>
2013-03-02 16:08:46 +0100

    refactor specs

--- spec/integration/draper_spec.rb
+++ spec/integration/draper_spec.rb
@@ -2,13 +2,15 @@ require 'fixtures/fake_rails_application'
 require 'rspec/rails'
 
 describe AttendeesController, type: :controller do
-  it "should be a decorator" do
+  before do
     get '/attendee/dave'
+  end
+
+  it "should be a decorator" do
     controller.view_context.attendee.should be_instance_of(AttendeeDecorator)
   end
 
   it "should be a decorator in collection" do
-    get '/attendee/dave'
     controller.view_context.attendees.each do |attendee|
       attendee.should be_instance_of(AttendeeDecorator)
     end
--- spec/integration/rails_application_spec.rb
+++ spec/integration/rails_application_spec.rb
@@ -2,24 +2,24 @@ require 'fixtures/fake_rails_application'
 require 'rspec/rails'
 
 describe ConferencesController, type: :controller do
+  before do
+    get '/conference/RuPy'
+  end
+
   describe "inside a view" do
     it "should be a decorator" do
-      get '/conference/RuPy'
       controller.view_context.conference.should be_instance_of(ConferenceDecorator)
     end
 
     it "should have decorated conference" do
-      get '/conference/RuPy'
       controller.view_context.conference.decorated_object.should be_instance_of(Conference)
     end
 
     it "should be specified decorator" do
-      get '/conference/RuPy'
       controller.view_context.other_conference.should be_instance_of(CoolConferenceDecorator)
     end
 
     it "should cache decorator" do
-      get '/conference/RuPy'
       controller.view_context.conference.object_id.should == controller.view_context.conference.object_id
     end
 
@@ -30,12 +30,10 @@ describe ConferencesController, type: :controller do
 
   describe "inside a controller" do
     it "should be undecorated object" do
-      get '/conference/RuPy'
       controller.conference.should be_instance_of(Conference)
     end
 
     it "should not be specified decorator" do
-      get '/conference/RuPy'
       controller.other_conference.should be_instance_of(Conference)
     end
   end
